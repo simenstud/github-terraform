3a9599f3501782f573486105d4ba8b407beb2a68
Simen <simendju@stud.ntnu.no>
2024-09-24 03:39:57 +0200

    created rg

---
+++ terraform/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "4.3.0"
+        }
+    }
+
+    backend "azurerm" {
+        resource_group_name  = "rg-backend-tfstate"
+        storage_account_name = "sabetfstrsfv2luge"
+        container_name       = "tfstate"
+        key                  = "rg.terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "a3adf20e-4966-4afb-b717-4de1baae6db1"
+}
